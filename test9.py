6eb2ce374cb7554786756db6d4b8c2224ad8143c
janweesha <brunoxia@outlook.com>
2018-07-24 22:43:22 +0800

    练习9
    python实现（练习1和练习9混了） 1.定义一个天气温度列表，写出里面每一天的温度 2.打印一周的天气，天气里面的周三打印例外：周三+温度。

---
+++ test9.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 23 09:36:10 2018
+
+@author: 10257
+"""
+temp=(20,28,25,30,31)
+for i in range(0,5):
+    if(i!=3) :
+        print(temp[i])
+    else:
+        print("周三温度是："+str(temp[i]))
+
+
+    
+
+               
+    
+    
+    
+
